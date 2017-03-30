7611eac06af1cd77ce55b221a3f26f9b594d0cf7
Gabriel <gabri@GABE-HP>
2017-03-30 15:06:58 +0100

    Finished Calculation

--- BMIcalulator/BMI_Calculate.cs
+++ BMIcalulator/BMI_Calculate.cs
@@ -2,7 +2,7 @@
 
 public partial class Calculate
 {
-	private double BMI(double height, double weight)
+	private double BMI(double h, double w)
     {
         double BMI = weight / height / height;
         return BMI;
